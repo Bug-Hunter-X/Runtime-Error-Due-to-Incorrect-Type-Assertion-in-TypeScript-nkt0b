a29e4a124c6cbd364302efdf06d8afd1c3ab9fc8
BugHunter-X <askzenai@gmail.com>
2025-02-11 00:56:05 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,13 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+let result1 = add(5, 3); // result1: number = 8
+let result2 = subtract(10, 4); // result2: number = 6
+
+// Incorrect type assertion
+let result3 = add("5", "3"); //result3: number = "53"  This will cause a runtime error
